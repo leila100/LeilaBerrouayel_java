aaa5bfd78958ca664d773ad86ef8c6570fdfdf63
Leila <nb.leila10@gmail.com>
2020-09-15 11:05:11 -0500

    Add simple user command access

--- LeilaBerrouayel_java.java
+++ LeilaBerrouayel_java.java
@@ -1,6 +1,23 @@
+import java.io.Console;
+
 public class LeilaBerrouayel_java {
 
   public static void main(String[] args) {
-    System.out.println("First setup");
+    Console console = System.console();
+
+        String input = "";
+        while (!"q".equalsIgnoreCase(input)) {
+
+            System.out.print("Enter something (q to quite): ");
+
+            input = console.readLine();
+            String[] arrOfStr = input.split(" "); 
+  
+            for (String a : arrOfStr) 
+                System.out.println(a); 
+            System.out.println("input : " + input);
+        }
+
+        System.out.println("bye bye!");
   }
 }
